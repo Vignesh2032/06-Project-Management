c9e806509b63a2e0468856838548702791527a81
Vignesh G <113256226+Vignesh2032@users.noreply.github.com>
2024-12-09 09:45:55 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -114,6 +114,7 @@ function App() {
                 onStartAddProject={handleStartAddProject}
                 projects={projectsState.projects}
                 onSelectProject={handleSelectProject}
+                selectedProjectId={projectsState.selectedProjectId}
             />
             {content}
         </main>
